5e3ddf5afdea9225a29420a927e8690ce9f1cffd
sophieouioui <sophieouiouioui@gmail.com>
2023-03-28 22:10:21 +0300

    finished js file and modified css file

---
+++ main.js
@@ -0,0 +1,12 @@
+function changeColors(){
+    var hexColors = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+    var hexCode = ''
+
+    for(var i = 0; i < 6; i ++){
+        var randomIndex = Math.floor(Math.random() * hexColors.length)
+        hexCode += hexColors[randomIndex]
+    }
+
+    document.getElementById('hex-code').innerHTML = hexCode
+    document.getElementsByTagName('body')[0].style.background = '#' + hexCode
+}
